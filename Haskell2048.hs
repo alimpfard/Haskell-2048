78731beb88b109190f792812d759acbd5d2d1cb4
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-08-16 22:40:54 -0400

    Reformatting code

--- Haskell2048.hs
+++ Haskell2048.hs
@@ -1,29 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
 
-import Prelude hiding (Right, Left)
-
-import Control.Monad (liftM)
-import Data.List     (elemIndices, transpose)
+import Data.List     (elemIndices, intercalate, transpose)
 import System.IO     (BufferMode(..), hSetBuffering, stdin)
 import System.Random (randomRIO)
-
 
 -- Each inner list is a row, starting with the top row
 -- A 0 is an empty tile
 type Board = [[Int]]
 
-data Direction = Up | Right | Down | Left
-
-charToDirection :: Char -> Maybe Direction
-charToDirection 'w' = Just Up
-charToDirection 'd' = Just Right
-charToDirection 's' = Just Down
-charToDirection 'a' = Just Left
-charToDirection _   = Nothing
+data Direction = North | East | South | West
 
 slideLeft :: Board -> Board
 slideLeft = map slideRow
-  where slideRow [] = []
+  where slideRow [ ] = [ ]
         slideRow [x] = [x]
         slideRow (x:y:zs)
           | x == 0 = slideRow (y : zs) ++ [0]
@@ -32,13 +21,14 @@ slideLeft = map slideRow
           | otherwise = x : slideRow (y : zs)
 
 slide :: Direction -> Board -> Board
-slide Up    = transpose . slideLeft . transpose
-slide Left  = slideLeft
-slide Right = mirror . slideLeft . mirror
-slide Down  = transpose . mirror . slideLeft . mirror . transpose
+slide North = transpose . slideLeft . transpose
+slide East  = map reverse . slideLeft . map reverse
+slide South = transpose . map reverse . slideLeft . map reverse . transpose
+slide West  = slideLeft
 
-mirror :: Board -> Board
-mirror = map reverse
+-- Tells us if the player won the game by getting a 2048 tile
+completed :: Board -> Bool
+completed b = any (elem 2048) b
 
 -- Tells us if the game is over because there are no valid moves left
 stalled :: Board -> Bool
@@ -47,7 +37,7 @@ stalled b = all stalled' b && all stalled' (transpose b)
         noNeighbors [ ] = True
         noNeighbors [_] = True
         noNeighbors (x:y:zs)
-          | x == y = False
+          | x == y    = False
           | otherwise = noNeighbors (y:zs)
 
 -- Returns tuples of the indices of all of the empty tiles
@@ -60,45 +50,45 @@ updateTile :: (Int, Int) -> Int -> Board -> Board
 updateTile (rowI, columnI) value = updateIndex (updateIndex (const value) columnI) rowI
   where updateIndex fn i list = take i list ++ fn (head $ drop i list) : tail (drop i list)
 
--- Returns a new value for adding to the board
-pickNewValue :: IO Int
-pickNewValue = liftM (\x -> if x == 1 then 4 else 2) $ randomRIO (1, 10 :: Int)
-
--- Picks an empty tile to place the new one onto
-pickNewTileIndex :: Board -> IO (Int, Int)
-pickNewTileIndex b = liftM (tiles !!) $ randomRIO (0, length tiles - 1)
-  where tiles = emptyTiles b
-
 -- Adds a tile to a random empty spot.
 -- 90% of the time the tile is a 2, 10% of the time it is a 4
 addTile :: Board -> IO Board
 addTile b = do
-    index <- pickNewTileIndex b
-    value <- pickNewValue
-    return $ updateTile index value b
+  let tiles = emptyTiles b
+  newPoint <- randomRIO (0, length tiles - 1) >>= return . (tiles !!)
+  newValue <- randomRIO (1, 10 :: Int) >>= return . \x -> if x == 1 then 4 else 2
+  return $ updateTile newPoint newValue b
 
 -- Our main game loop
 gameloop :: Board -> IO ()
 gameloop b = do
+    putStrLn "---------------------"
     putStrLn $ boardToString b
-    input <- getChar
-    putStrLn ""
-    let b' = maybe b (`slide` b) $ charToDirection input
-    if stalled b'
-        then print "Game over."
-        else if b' == b -- Only add a new tile if we made a change when sliding
-            then gameloop b'
-            else addTile b' >>= gameloop
+    putStrLn "---------------------"
+    if stalled b
+        then putStrLn "Game over."
+        else if completed b
+            then putStrLn "You won!"
+            else do
+              input <- getChar
+              putStrLn "" -- So that the board is correctly aligned next time we print it
+              let b1 = maybe b (`slide` b) $ lookup input $ zip "wasd" [North, West, South, East]
+              if b1 == b -- Only add a new tile if we made a change when sliding
+                  then gameloop b1
+                  else addTile b1 >>= gameloop
 
 -- Board pretty printing
 boardToString :: Board -> String
-boardToString = unlines . map show
+boardToString = init . unlines . map (vertical . map (pad . showSpecial))
+  where vertical = ('|' :) . (++ "|") . intercalate "|"
+        showSpecial 0 = ""
+        showSpecial n = show n
+        pad s = replicate (4 - length padTemp) ' ' ++ padTemp
+          where padTemp = s ++ if length s < 3 then " " else ""
 
 main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
-    let row = [0,0,0,0]
-    let board = [row,row,row,row]
-    b1 <- addTile board
-    b2 <- addTile b1 -- Add two tiles randomly and start the game!
-    gameloop b2
+    let board = replicate 4 (replicate 4 0)
+    b1 <- addTile board >>= addTile -- Add two tiles randomly and start the game!
+    gameloop b1
